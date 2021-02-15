1181dcd353c60e80d4b3dcd5474260bc592798aa
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-02-15 18:09:13 -0500

    connected to client

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
+    path('products/<str:pk>/', views.getProduct, name="product"),
+]
     
-}
--- ecommerce_server/settings.py
+++ ecommerce_server/settings.py
@@ -31,17 +31,24 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+        
+    'rest_framework',
+    'corsheaders',
     
-    'base.apps.BaseConfig'
+    'base.apps.BaseConfig',
+    
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
